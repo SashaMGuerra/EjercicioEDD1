ea1ff350b4075d003d325c17947a34bc2670deab
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2020-11-13 11:36:01 +0100

    Initial commit

---
+++ src/ejercicioedd1/EjercicioEDD1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioedd1;
+
+/**
+ * Muestra un mensaje por pantalla que dice "Hola, Mundo".
+ * @author Sasha
+ * @version 1.0
+ */
+public class EjercicioEDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.("Hola, Mundo");
+    }
+    
+}
